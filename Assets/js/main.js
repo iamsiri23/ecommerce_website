7dde184d6cde30d3067337a94673a2ba300a8a97
iamsiri23 <sirithirumagal998@gmail.com>
2021-01-22 11:41:35 +0530

    add code

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,5 +1,5 @@
 const showMenu=(toggleId,navId)=>{
-    const toggle=document.getElementByID(toggleId),
+    const toggle=document.getElementById(toggleId),
     nav=document.getElementById(navId)
 
     if(toggle && nav){
